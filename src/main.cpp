b3d0b45e7b0a7bb7452fd355778e78b44c73e073
DinoDieter <dieter.brueggemann@gmx.de>
2018-02-06 14:37:30 +0100

    ssss

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,8 @@
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(115200);
-<<<<<<< HEAD
-    //Hallo
-=======
-    // Test 22 
->>>>>>> 480fc64009c29ab87bb9610432f4b1ae07501037
+     // Test 22 
+
 }
 
 void loop() {
